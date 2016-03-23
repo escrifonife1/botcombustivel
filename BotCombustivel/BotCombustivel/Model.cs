ff874b09dc4135f1490c51166eb20d846bafbdaf
tiagod <tiagod@takenet.com.br>
2016-03-23 19:39:09 -0300

    conectando no meuspostos

---
+++ BotCombustivel/BotCombustivel/Model.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCombustivel
+{
+    public class Regioes
+    {
+        public class Cidade
+        {
+            public string cd_cidade { get; set; }
+            public string nome { get; set; }
+            public string nome_tela { get; set; }
+            public string estado { get; set; }
+            public string cd_regiao { get; set; }
+            public string nome_regiao { get; set; }
+            public string nome_tela_regiao { get; set; }
+            public string cidade_sede { get; set; }
+        }
+
+        public class Regio
+        {
+            public string cd_regiao { get; set; }
+            public string nome { get; set; }
+            public string nome_tela { get; set; }
+            public string cd_cidade_sede { get; set; }
+            public string nome_cidade_sede { get; set; }
+            public List<Cidade> cidades { get; set; }
+        }
+
+        public class Data
+        {
+            public List<Regio> regioes { get; set; }
+            public string status { get; set; }
+            public string detail { get; set; }
+            public string resquest_uri { get; set; }
+            public string created_at { get; set; }
+            public string elapsed_time { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Data data { get; set; }
+        }
+    }
+
+    public class Buscar
+    {
+        public class Busca
+        {
+            public string posto { get; set; }
+            public string nome { get; set; }
+            public string endereco { get; set; }
+            public string bairro { get; set; }
+            public string dt_pesquisa { get; set; }
+            public string bandeira { get; set; }
+            public string icone { get; set; }
+            public string gasolina { get; set; }
+            public string in_gasolina { get; set; }
+            public string alcool { get; set; }
+            public string in_alcool { get; set; }
+            public string diesel { get; set; }
+            public string in_diesel { get; set; }
+            public string gnv { get; set; }
+            public string in_gnv { get; set; }
+            public string latitude { get; set; }
+            public string longitude { get; set; }
+            public int distancia { get; set; }
+        }
+
+        public class Data
+        {
+            public IList<Busca> busca { get; set; }
+            public string status { get; set; }
+            public string detail { get; set; }
+            public string resquest_uri { get; set; }
+            public DateTime created_at { get; set; }
+            public string elapsed_time { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Data data { get; set; }
+        }
+    }
+}
--- BotCombustivel/BotCombustivel/PlainTextMessageReceiver.cs
+++ BotCombustivel/BotCombustivel/PlainTextMessageReceiver.cs
@@ -1,5 +1,8 @@
 using Lime.Protocol;
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Takenet.MessagingHub.Client;
 using Takenet.MessagingHub.Client.Receivers;
@@ -10,8 +13,22 @@ namespace BotCombustivel
     {
         public override async Task ReceiveAsync(Message message)
         {
+            var httpClient = new HttpClient();
+            var webClient = new WebClient();
+            webClient.Headers.Add("Content-Type", "application/json");
+            webClient.Headers.Add("OSLC-Core-Version", "2.0");
+            var buscaJson = webClient.DownloadString($"http://api.meuspostos.com.br/busca.json?chave=" + message.Content.ToString());
+            var busca = Newtonsoft.Json.JsonConvert.DeserializeObject<Buscar.RootObject>(buscaJson);
+            /*
+             //Regiões
+            var regioesStr = webClient.DownloadString($"http://api.meuspostos.com.br/regioes.json");
+            var regioes = Newtonsoft.Json.JsonConvert.DeserializeObject<Regioes.RootObject>(regioesStr);
+            */
+            await EnvelopeSender.SendMessageAsync($"Recebido {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}", message.From);
+            
             Console.WriteLine($"From: {message.From} \tContent: {message.Content}");
-            await EnvelopeSender.SendMessageAsync("Pong!", message.From);
+            
+            //await EnvelopeSender.SendMessageAsync("Pong!", message.From);
             await EnvelopeSender.SendNotificationAsync(message.ToConsumedNotification());
         }
     }
