7c266a7a3b1df76174b233adab2db03fe694462f
tiagod <tiagod@takenet.com.br>
2016-03-23 16:23:11 -0300

    Conectando

---
+++ BotCombustivel/BotCombustivel.Test/BotConnectionTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BotCombustivel.Test
+{
+    [TestClass]
+    public class BotConnectionTest
+    {
+        [TestMethod]
+        public async Task TestConnection()
+        {
+            var botConnection = new BotConnection();
+            await botConnection.Connect();
+        }
+    }
+}
---
+++ BotCombustivel/BotCombustivel/BotConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Takenet.MessagingHub.Client;
+
+namespace BotCombustivel
+{
+    public class BotConnection
+    {
+        public async Task Connect()
+        {
+            const string login = "BOTCOMBUSTIVEL"; //Identificador da aplicação
+            const string accessKey = "OVJ1MGRs"; //Chave de acesso da aplicação
+            var client = new MessagingHubClientBuilder()
+                .UsingAccessKey(login, accessKey)
+                .WithSendTimeout(TimeSpan.FromSeconds(20))
+                .Build();
+            var messageText = "It is working bitch!";
+            try
+            {
+                await client.StartAsync();
+
+                await client.SendMessageAsync(messageText, to: login);
+
+                using (var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(30)))
+                {
+                    var message = await client.ReceiveMessageAsync(cancellationToken.Token);
+
+                    if (!message.Content.ToString().Equals(messageText))
+                    {
+                        throw new Exception("Não funcionou");
+                    }
+                }
+
+                await client.StopAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
