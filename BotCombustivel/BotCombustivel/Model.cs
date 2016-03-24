30d157acf5cbd579e4802ad24dfcdc6f42b5a86e
tiagod <tiagod@takenet.com.br>
2016-03-24 11:44:21 -0300

    Criando modelos para consumir o meuspostos

--- BotCombustivel/BotCombustivel/Model.cs
+++ BotCombustivel/BotCombustivel/Model.cs
@@ -85,4 +85,51 @@ namespace BotCombustivel
             public Data data { get; set; }
         }
     }
+
+    public class Postos
+    {
+        public class Posto
+        {
+            public string posto { get; set; }
+            public string nome { get; set; }
+            public string telefone { get; set; }
+            public string endereco { get; set; }
+            public string bairro { get; set; }
+            public string dt_pesquisa { get; set; }
+            public string bandeira { get; set; }
+            public string icone { get; set; }
+            public string latitude { get; set; }
+            public string longitude { get; set; }
+            public string gasolina { get; set; }
+            public string nota_gasolina { get; set; }
+            public string alcool { get; set; }
+            public string in_alcool { get; set; }
+            public string diesel { get; set; }
+            public string in_diesel { get; set; }
+            public string gnv { get; set; }
+            public string in_gnv { get; set; }
+            public string qt_nota1 { get; set; }
+            public string qt_nota2 { get; set; }
+            public string qt_nota3 { get; set; }
+            public string qt_nota4 { get; set; }
+            public string qt_nota5 { get; set; }
+            public object comentarios { get; set; }
+        }
+
+        public class Data
+        {
+            public IList<Posto> postos { get; set; }
+            public string status { get; set; }
+            public string detail { get; set; }
+            public string resquest_uri { get; set; }
+            public DateTime created_at { get; set; }
+            public string elapsed_time { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Data data { get; set; }
+        }
+
+    }
 }
--- BotCombustivel/BotCombustivel/PlainTextMessageReceiver.cs
+++ BotCombustivel/BotCombustivel/PlainTextMessageReceiver.cs
@@ -1,6 +1,7 @@
 using Lime.Protocol;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,25 +12,71 @@ namespace BotCombustivel
 {
     public class PlainTextMessageReceiver : MessageReceiverBase
     {
-        public override async Task ReceiveAsync(Message message)
+        /*public override async Task ReceiveAsync(Message message)
         {
-            var httpClient = new HttpClient();
             var webClient = new WebClient();
             webClient.Headers.Add("Content-Type", "application/json");
             webClient.Headers.Add("OSLC-Core-Version", "2.0");
             var buscaJson = webClient.DownloadString($"http://api.meuspostos.com.br/busca.json?chave=" + message.Content.ToString());
             var busca = Newtonsoft.Json.JsonConvert.DeserializeObject<Buscar.RootObject>(buscaJson);
+            */
             /*
              //Regiões
             var regioesStr = webClient.DownloadString($"http://api.meuspostos.com.br/regioes.json");
             var regioes = Newtonsoft.Json.JsonConvert.DeserializeObject<Regioes.RootObject>(regioesStr);
             */
-            await EnvelopeSender.SendMessageAsync($"Recebido {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}", message.From);
+            /*await EnvelopeSender.SendMessageAsync($"Recebido {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}", message.From);
             
             Console.WriteLine($"From: {message.From} \tContent: {message.Content}");
             
             //await EnvelopeSender.SendMessageAsync("Pong!", message.From);
             await EnvelopeSender.SendNotificationAsync(message.ToConsumedNotification());
+        }*/
+
+        public override async Task ReceiveAsync(Message message)
+        {
+             var searchCity = new SearchCity();
+
+             await EnvelopeSender.SendMessageAsync($"Recebido {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}", message.From);
+
+            Console.WriteLine($"From: {message.From} \tContent: {message.Content}");
+
+            //await EnvelopeSender.SendMessageAsync("Pong!", message.From);
+            await EnvelopeSender.SendNotificationAsync(message.ToConsumedNotification());
         }
+    }
+
+    public class SearchCity : SearchBase
+    {
+        public void Search(Message message)
+        {
+            
+            var buscaJson = Client.DownloadString($"http://api.meuspostos.com.br/busca.json?chave=" + message.Content.ToString());
+            var busca = Newtonsoft.Json.JsonConvert.DeserializeObject<Buscar.RootObject>(buscaJson);
+
+            if (busca.data.busca.Any())
+            {
+                foreach (var b in busca.data.busca)
+                {
+                    var postosJson =
+                        Client.DownloadString($"http://api.meuspostos.com.br/posto.json?posto=" + b.posto);
+                    var posto = Newtonsoft.Json.JsonConvert.DeserializeObject<Postos.RootObject>(buscaJson);
+                }
+            }
+        }
+    }
+
+    public abstract class SearchBase
+    {
+        public WebClient Client { get; private set; }
+
+        protected SearchBase()
+        {
+            Client = new WebClient();
+            Client.Headers.Add("Content-Type", "application/json");
+            Client.Headers.Add("OSLC-Core-Version", "2.0");
+        }
+
+        
     }
 }
