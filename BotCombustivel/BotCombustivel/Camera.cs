7f33147cb3e870511acc1f5aec8f3e9cc896abc3
Gustavo Castro <gustavo@takenet.com.br>
2016-03-24 14:01:44 -0300

    entity Camera Created

---
+++ BotCombustivel/BotCombustivel/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCombustivel
+{
+    public class Camera
+    {
+        private string _name { get; set;}
+        private string _url { get; set; }
+        private Array _coordinates { get; set; }
+        private string _address { get; set; }
+
+        public Camera(string name, string url, string address)
+        {
+            _name = name;
+            _url = url;
+            _address = address;
+        }
+
+        
+    }
+}
